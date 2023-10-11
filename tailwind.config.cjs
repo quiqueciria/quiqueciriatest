ab134974fcff44d6aa2a6e699675073db95f23fc
Quique Ciria <quiqueciria@gmail.com>
2023-10-11 11:55:54 +0200

    normalized to Astro components and simplified styles

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,9 +3,7 @@ module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
     extend: {
-      colors: {
-        greenlight: "#42f563",
-      },
+      colors: {},
     },
   },
   plugins: [],
